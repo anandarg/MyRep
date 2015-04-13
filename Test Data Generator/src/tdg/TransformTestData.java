468115960cf580add2b5054ad3ea28c97fd980f7
anandarg <anandarg@yahoo.com>
2015-04-12 20:11:14 -0400

    Integrated to work with the latest version of the CRIM tools.

--- Test Data Generator/src/tdg/TransformTestData.java
+++ Test Data Generator/src/tdg/TransformTestData.java
@@ -29,7 +29,7 @@ public class TransformTestData {
             BufferedWriter writer = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
             writer.append("group,");
     	    writer.append("case,case,case,case,,");
-    	    writer.append("calibration,calibration,calibration,");
+    	    //writer.append("calibration,calibration,calibration,");
     	    writer.append("step,step,step,step,step,step");
     	    
     	    BufferedReader br = new BufferedReader(new FileReader(srcTestDataFile));
@@ -53,10 +53,11 @@ public class TransformTestData {
     	    	}
     	    }
     	    br.close();
+    	    writer.append(",output,function");
     	    writer.append('\n');	
     	    writer.append("name,");
     	    writer.append("case,name,mode,description,Calibration set,");
-    	    writer.append("KaAIRD_p_APPD_BaroDiffHi,KeAIRC_t_MaxPumpOffDly,KeAIRD_Cnt_NumOfPresSnsr,");
+    	    //writer.append("KaAIRD_p_APPD_BaroDiffHi,KeAIRC_t_MaxPumpOffDly,KeAIRD_Cnt_NumOfPresSnsr,");
     	    writer.append("sequence,description,name,time input,time function,time output");
     	    br = new BufferedReader(new FileReader(srcTestDataFile));
     	    cycleStart = 0;
@@ -77,10 +78,11 @@ public class TransformTestData {
     	    		writer.append(","+ line.substring(0, line.lastIndexOf('=')));
     	    	}
     	    }
+    	    writer.append(",tolerance");
     	    br.close();
     	    
     	    br = new BufferedReader(new FileReader(srcTestDataFile));
-    	    
+    	    int stepctr = 0;
     	    while ((line = br.readLine()) != null) {
     	      
     	    	if (line.trim().length() == 0) {
@@ -88,9 +90,13 @@ public class TransformTestData {
     	    	}
     	    	else if (line.contains(cs)){
     	    		writer.append('\n');	
-    	        	writer.append(",,,,,,,,,,,");
-    	        	writer.append(line);
-    	        	writer.append(",,,");
+    	    		if(stepctr == 0)
+    	    			writer.append("TDG,case,TDG_TEST,behavioral,,,,,");
+    	    		else
+    	    			writer.append(",,,,,,,,");
+    	        	//writer.append(line);
+    	    		writer.append("Step_"+stepctr);
+    	        	writer.append(","+stepctr+","+stepctr+","+stepctr++);
     	    	}
     	    	else if (!line.contains(cs)){
     	    		if(line.substring(line.lastIndexOf('=')+1,line.lastIndexOf('=')+2).equals("{")){
